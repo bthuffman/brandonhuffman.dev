3121a82624f814764d9f8de5cfc5e74e571a65e4
Brandon Huffman <bt_huffman@msn.com>
2019-01-17 19:39:58 -0700

    Updated the index, created a script.js and script.html for mathematical understanding of my learning style. Will look up how to use get document by id and inner html to write these numbers into my pro dev doc index.html.

---
+++ script.js
@@ -0,0 +1,10 @@
+let auditory = 3;
+let visual = 4;
+let tactile = 5;
+let total = auditory + visual + tactile;
+document.write( "Denominator: " + total + "<br>");
+
+let auditoryPercent = auditory/total;
+let visualPercent = visual/total;
+let tactilePercent = tactile/total;
+document.write("Auditory: " + auditoryPercent + "<br>" + "Visual: " + visualPercent + "<br>" + "Tactile: " + tactilePercent + "<br>");
