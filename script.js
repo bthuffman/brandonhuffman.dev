b8c69fff2fe6a78ddf27efc08dd0101df75400ae
Brandon Huffman <bt_huffman@msn.com>
2019-01-18 11:17:20 -0700

    Updated the index and javascript file to post mathematical calculations for my learning style into the innerHtml.

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let auditory = 3;
 let visual = 4;
 let tactile = 5;
 let total = auditory + visual + tactile;
-document.write( "Denominator: " + total + "<br>");
+// document.write( "Denominator: " + total + "<br>");
 
-let auditoryPercent = auditory/total;
-let visualPercent = visual/total;
-let tactilePercent = tactile/total;
-document.write("Auditory: " + auditoryPercent + "<br>" + "Visual: " + visualPercent + "<br>" + "Tactile: " + tactilePercent + "<br>");
+let tactilePercent = Math.round(tactile/total*100);
+let visualPercent = Math.round(visual/total*100);
+let auditoryPercent = Math.round(auditory/total*100);
+console.log("Auditory: " + auditoryPercent + "<br>" + "Visual: " + visualPercent + "<br>" + "Tactile: " + tactilePercent + "<br>");
+
+document.getElementById("tactile").innerHTML = tactilePercent.toString();
+document.getElementById("visual").innerHTML = visualPercent.toString();
+document.getElementById("auditory").innerHTML = auditoryPercent.toString();
