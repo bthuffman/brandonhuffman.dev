1480d9325fac376824154c2f5ac847d30a03c44a
Brandon Huffman <bt_huffman@msn.com>
2019-03-05 13:35:12 -0700

    New refurbished files moved here, replacing the old.

--- pro-dev-refurbished/js/script.js
+++ pro-dev-refurbished/js/script.js
@@ -1,5 +1,5 @@
-let auditory = 3;
-let visual = 4;
+let auditory = 4;
+let visual = 7;
 let tactile = 5;
 let total = auditory + visual + tactile;
 // document.write( "Denominator: " + total + "<br>");
@@ -7,8 +7,10 @@ let total = auditory + visual + tactile;
 let tactilePercent = Math.round(tactile/total*100);
 let visualPercent = Math.round(visual/total*100);
 let auditoryPercent = Math.round(auditory/total*100);
-console.log("Auditory: " + auditoryPercent + "<br>" + "Visual: " + visualPercent + "<br>" + "Tactile: " + tactilePercent + "<br>");
+console.log("Auditory: " + auditoryPercent + "<br/>" + "Visual: " + visualPercent + "<br/>" + "Tactile: " + tactilePercent + "<br/>");
 
 document.getElementById("tactile").innerText = tactilePercent.toString();
 document.getElementById("visual").innerText = visualPercent.toString();
 document.getElementById("auditory").innerText = auditoryPercent.toString();
+
+
